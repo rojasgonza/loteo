8bae7fe5e5f1f4fd47ddfd4b4997f23dc89fef84
rojasgonza <rojasgonzalo23@gmail.com>
2025-09-11 06:39:26 -0300

    errores

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://origins-garmin-dave-owen.trycloudflare.com';
+export const API_URL = 'https://particles-rebates-members-inches.trycloudflare.com';

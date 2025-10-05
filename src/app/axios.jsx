fec106e0802a28a0a7eb205aec1964daffe6ead6
rojasgonza <rojasgonzalo23@gmail.com>
2025-10-05 10:32:51 -0300

    correccion backend

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://dirt-priest-broadcast-mapping.trycloudflare.com';
+export const API_URL = 'https://partner-aspect-sen-throw.trycloudflare.com';

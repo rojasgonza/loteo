027f7b40089185d14fb60e35fa6000182787c923
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-10 08:31:48 -0300

    correccion front

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://atlantic-sunglasses-viewing-region.trycloudflare.com';
+export const API_URL = 'https://deal-douglas-sunday-act.trycloudflare.com';

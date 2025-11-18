1bcb1fba9abe6c3333163d22c2febfc06bc2a7f1
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-18 06:28:06 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://compression-jump-easily-mas.trycloudflare.com';
+export const API_URL = 'https://vid-employers-happen-organizer.trycloudflare.com';

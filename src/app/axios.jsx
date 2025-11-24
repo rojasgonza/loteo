e558492afc60da874cd6297eb2c5be416b30f717
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-24 08:11:14 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://breakfast-adding-logical-testament.trycloudflare.com';
+export const API_URL = 'https://opposition-blake-jon-institutions.trycloudflare.com';

5f53581e396d89a5f2076553a0242785e89b6f2d
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-24 08:01:36 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://vid-employers-happen-organizer.trycloudflare.com';
+export const API_URL = 'https://breakfast-adding-logical-testament.trycloudflare.com';

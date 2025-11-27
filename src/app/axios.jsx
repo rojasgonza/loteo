40ec11fd51b3e4e8f0779df2d7f8c2534a6b33d6
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-27 13:04:25 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://sticky-subsidiary-librarian-boats.trycloudflare.com';
+export const API_URL = 'https://reproduce-supposed-proof-acquisitions.trycloudflare.com';

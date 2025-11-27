b281e50d74051c2fa75939b43f540ff48e03261a
rojasgonza <rojasgonzalo23@gmail.com>
2025-11-27 13:00:41 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://opposition-blake-jon-institutions.trycloudflare.com';
+export const API_URL = 'https://sticky-subsidiary-librarian-boats.trycloudflare.com';

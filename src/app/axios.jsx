739f05f748bd76d0087f91ef5f1904d7d482745c
rojasgonza <rojasgonzalo23@gmail.com>
2025-12-08 09:50:44 -0300

    correccion axios

--- src/app/axios.jsx
+++ src/app/axios.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://reproduce-supposed-proof-acquisitions.trycloudflare.com';
+export const API_URL = 'https://spiritual-startup-matter-extended.trycloudflare.com';
